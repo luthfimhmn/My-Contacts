4cd47aa86fd58da66804851c4d5c68ffa109ca63
Luthfi Muhaimin <luthfimhmn@gmail.com>
2021-03-30 11:53:36 +0700

    Change FormUser.jsx to functional component

--- my-app/src/components/FormUser.jsx
+++ my-app/src/components/FormUser.jsx
@@ -1,62 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class UserForm extends React.Component {
-  constructor () {
-    super()
-    this.state = {
-      first_name: '',
-      last_name: '',
-      email: ''
-    }
+function UserForm (props) {
+  const [first_name, setFirst_name] = useState('')
+  const [last_name, setLast_name] = useState('')
+  const [email, setEmail] = useState('')
+
+
+  function formOnSubmit (event) {
+    event.preventDefault();
+    props.addUser({first_name, last_name, email})
   }
 
-  handleOnChange = (event) => {
-    this.setState({
-      ...this.state,
-      [event.target.name]: event.target.value
-    })
-  }
+  return (
+    <form onSubmit={e => formOnSubmit(e)}>
+      <label htmlFor="first_name">First Name :</label><br/>
+      <input
+        id="name"
+        name="first_name"
+        type="text"
+        value={first_name}
+        onChange={e => setFirst_name(e.target.value)}
+      />
+      <br/>
 
-  formOnSubmit = (event) => {
-    event.preventDefault()
-    this.props.addUser(this.state)
-  }
+      <label htmlFor="last_name">Last Name :</label><br/>
+      <input
+        id="last_name"
+        type="text"
+        name="last_name"
+        value={last_name}
+        onChange={e => setLast_name(e.target.value)}
+      /><br/>
 
-  render () {
-    return (
-      <form onSubmit={this.formOnSubmit}>
-        <label htmlFor="first_name">First Name :</label><br/>
-        <input
-          id="name"
-          name="first_name"
-          type="text"
-          value={this.state.first_name}
-          onChange={this.handleOnChange}
-        />
-        <br/>
+      <label htmlFor="email">Email :</label><br/>
+      <input
+        id="email"
+        type="text"
+        name="email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      /><br/>
 
-        <label htmlFor="last_name">Last Name :</label><br/>
-        <input
-          id="last_name"
-          type="text"
-          name="last_name"
-          value={this.state.last_name}
-          onChange={this.handleOnChange}
-        /><br/>
-
-        <label htmlFor="email">Email :</label><br/>
-        <input
-          id="email"
-          type="text"
-          name="email"
-          value={this.state.email}
-          onChange={this.handleOnChange}
-        /><br/>
-
-        <button>submit</button>
-      </form>
-    )
-  }
+      <button>submit</button>
+    </form>
+  )
 }
 
 export default UserForm
